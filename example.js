29683054195dfee2acdd2bd8b40723be79aee511
Jakob Borg <jakob@nym.se>
2012-06-16 11:49:29 +0200

    Basics for running vpnc.

---
+++ example.js
@@ -0,0 +1,42 @@
+var vpnc = require('/usr/local/lib/node_modules/vpnc');
+var config = {
+    IPSec_ID: 'foobar',
+    IPSec_gateway: 'vpn.example.com',
+    IPSec_secret: 'topSecret',
+
+    Xauth_username: 'test@example',
+    Xauth_password: 'r00barb',
+
+    IKE_Authmode: 'psk',
+    IKE_DH_Group: 'dh2',
+    DNSUpdate: 'no',
+    NAT_Traversal_Mode: 'force-natt',
+    Local_Port: 0,
+    Cisco_UDP_Encapsulation_Port: 0,
+};
+
+function connect() {
+    vpnc.connect(config, function (err, code) {
+        if (err) {
+            console.log('Error connecting VPN:');
+            console.log(err);
+        } else {
+            console.log('VPN connected. Disconnecting in five seconds.');
+            setTimeout(disconnect, 5000);
+        }
+    });
+}
+
+function disconnect() {
+    vpnc.disconnect(function (err, code) {
+        if (err) {
+            console.log('Error isconnecting VPN:');
+            console.log(err);
+        } else {
+            console.log('VPN disconnected.');
+        }
+    });
+}
+
+connect();
+
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var vpncConfig = require('./lib/vpnc-config');
 var spawn = require('child_process').spawn;
+var tempFile = require('temp').path;
+var writeFile = require('fs').writeFile;
+
+var vpncConfig = require('./lib/vpnc-config');
 
 exports = module.exports = {
     connect: connect,
@@ -7,15 +10,26 @@ exports = module.exports = {
 };
 
 function connect(config, callback) {
-    // Load kernel module if necessary
+    // FIXME: Load kernel module if necessary
+
     // Create a config file for vpnc
-    // Launch vpnc with sudo, enabling sudo to ask for password
-    // Check exit status and/or output
-    // Call callback with suitable parameters
+    var configFile = tempFile({ suffix: '.vpnc.conf' });
+    writeFile(configFile, vpncConfig(config), 'utf-8', function (err) {
+        if (err) {
+            return callback(err);
+        }
+
+        // Launch vpnc with sudo, enabling sudo to ask for password
+        var sudo = spawn('sudo', [ 'vpnc', configFile ], { customFds: [0, 1, 2] });
+        sudo.on('exit', function (code) {
+            callback(null, code);
+        });
+    });
 }
 
 function disconnect(callback) {
-    // Run vpnc-disconnect
-    // Check exit status and/or output
-    // Call callback with suitable parameters
+    var sudo = spawn('sudo', [ 'vpnc-disconnect' ], { customFds: [0, 1, 2] });
+    sudo.on('exit', function (code) {
+        callback(null, code);
+    });
 }
